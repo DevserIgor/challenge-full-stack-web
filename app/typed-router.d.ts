3170904d22eb5303d4a83c423d7bd88fdceed2f4
Igor Silva <devserigor@gmail.com>
2024-02-16 16:30:48 -0300

    feat: route fix

--- app/typed-router.d.ts
+++ app/typed-router.d.ts
@@ -42,7 +42,6 @@ declare module 'vue-router/auto/routes' {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/students/': RouteRecordInfo<'/students/', '/students', Record<never, never>, Record<never, never>>,
     '/students/add': RouteRecordInfo<'/students/add', '/students/add', Record<never, never>, Record<never, never>>,
-    '/students/edit.[ra]': RouteRecordInfo<'/students/edit.[ra]', '/students/edit/:ra', { ra: ParamValue<true> }, { ra: ParamValue<false> }>,
     '/StudentsList': RouteRecordInfo<'/StudentsList', '/StudentsList', Record<never, never>, Record<never, never>>,
   }
 }
