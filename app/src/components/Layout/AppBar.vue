bf29088c56a04a4fc92e4e3e44374133fb69b1df
Igor Silva <devserigor@gmail.com>
2024-02-16 16:28:29 -0300

    feat: confi list of students

---
+++ app/src/components/Layout/AppBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app-bar title="GrupoA">
+    <template v-slot:append>
+      <v-btn icon> Sair </v-btn>
+    </template>
+  </v-app-bar>
+</template>
---
+++ app/src/pages/students/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const searchstudents = (search: string) => {
+  console.log('searching students', search);
+};
+</script>
+<template>
+  <v-container class="w-100 px-0">
+    <v-card class="w-100">
+      <v-card-title>Alunos</v-card-title>
+      <v-card-text class="w-100 d-flex justify-space-between">
+        <Search @onSearch="searchstudents" />
+
+        <DefaultButton
+          icon="mdi-plus"
+          @onAddStudent="$router.push(`/students/add`)"
+        >
+          Adicionar aluno
+        </DefaultButton>
+      </v-card-text>
+      <TableCustom />
+    </v-card>
+  </v-container>
+</template>
